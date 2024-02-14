04439fdcdf8af370ab7eae1c287a3a069fbf7142
Victoro7 <90275418+Victoro7@users.noreply.github.com>
2024-02-13 16:25:44 -0800

    Initial Commit

--- src/Initialize.py
+++ src/Initialize.py
@@ -1,15 +1,15 @@
 def write_preference(name, reservoir, rnum, tip, tnum,
-                     plate, insert):
+                     plate, insert):                        # Initialize file, write user preferences to top of file
     with open("%s.gcode" % name, "w") as file:
-        file.write(";FLAVOR: Repetier\n")
-        file.write(";RESERVOIR TYPE: %s\n" % reservoir)
-        file.write(";NUMBER: %d\n" % rnum)
-        file.write(";TIP TYPE: %s\n" % tip)
-        file.write(";NUMBER: %d\n" % tnum)
-        file.write(";PLATE TYPE: %s\n" % plate)
-        file.write(";INSERT TYPE: %s\n" % insert)
+        file.write(";FLAVOR: Repetier\n")                   # Write firmware type
+        file.write(";RESERVOIR TYPE: %s\n" % reservoir)     # Write reagent reservoir type
+        file.write(";NUMBER: %d\n" % rnum)                  # Write # of reservoirs
+        file.write(";TIP TYPE: %s\n" % tip)                 # Write tip type
+        file.write(";NUMBER: %d\n" % tnum)                  # Write # of tips
+        file.write(";PLATE TYPE: %s\n" % plate)             # Write plate type
+        file.write(";INSERT TYPE: %s\n" % insert)           # Write insert type
 
 
-def home(name):
+def home(name):                                             # Write home axes command to file
     with open("%s.gcode" % name, "a") as file:
-        file.write("G28 ;home\n")
+        file.write("G28 ;home\n")                           # G28 is the Home command
