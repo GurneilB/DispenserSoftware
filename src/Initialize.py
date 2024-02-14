266945a2eb78cffb48a9332944da67a59a702f59
Victoro7 <90275418+Victoro7@users.noreply.github.com>
2024-02-14 08:18:48 -0800

    Added pick_tool function + documentation

--- src/Initialize.py
+++ src/Initialize.py
@@ -1,15 +1,50 @@
 def write_preference(name, reservoir, rnum, tip, tnum,
-                     plate, insert):                        # Initialize file, write user preferences to top of file
+                     plate, insert):
+    """
+    Initializes a G-code file. Writes user defined preferences to top of file
+
+    :param name: name of the file
+    :param reservoir: Type of reagent reservoir
+    :param rnum: # of reservoirs
+    :param tip: Type of tip
+    :param tnum: # of tips
+    :param plate: Type of culture plate
+    :param insert: Type of custom insert (EZ-Seed or 3-in-1)
+    """
     with open("%s.gcode" % name, "w") as file:
-        file.write(";FLAVOR: Repetier\n")                   # Write firmware type
-        file.write(";RESERVOIR TYPE: %s\n" % reservoir)     # Write reagent reservoir type
-        file.write(";NUMBER: %d\n" % rnum)                  # Write # of reservoirs
-        file.write(";TIP TYPE: %s\n" % tip)                 # Write tip type
-        file.write(";NUMBER: %d\n" % tnum)                  # Write # of tips
-        file.write(";PLATE TYPE: %s\n" % plate)             # Write plate type
-        file.write(";INSERT TYPE: %s\n" % insert)           # Write insert type
+        file.write(";FLAVOR: Repetier\n")
+        file.write(";RESERVOIR TYPE: %s\n" % reservoir)
+        file.write(";NUMBER: %d\n" % rnum)
+        file.write(";TIP TYPE: %s\n" % tip)
+        file.write(";NUMBER: %d\n" % tnum)
+        file.write(";PLATE TYPE: %s\n" % plate)
+        file.write(";INSERT TYPE: %s\n" % insert)
 
 
-def home(name):                                             # Write home axes command to file
+def home(name):
+    """
+    Writes the homing axes command to file. Necessary for printer initialization
+
+    :param name: name of the file
+    """
     with open("%s.gcode" % name, "a") as file:
-        file.write("G28 ;home\n")                           # G28 is the Home command
+        file.write("G28 ;home\n")  # G28 is the Home command
+
+
+def pick_tool(name, plate):
+    """
+    Writes the tool select command to file (picks between EZ-Seed and 3-in-1)
+
+    :param name: name of file
+    :param plate: type of plate
+    :return:
+    """
+
+    if plate == "EZ-Seed":
+        tool = 0
+    else:
+        tool = 1
+
+    with open("%s.gcode" % name, "a") as file:
+        file.write("T%d ;select %s tool \n"
+                   % (tool, plate))  # T is the select tool command
