34b16e1d56e7fe28e06bd23b13578150e5e9967a
Victoro7 <90275418+Victoro7@users.noreply.github.com>
2024-02-14 08:33:51 -0800

    Added set_absolute, set_relative, set_mm, initializaetion

--- src/Initialize.py
+++ src/Initialize.py
@@ -31,20 +31,78 @@ def home(name):
         file.write("G28 ;home\n")  # G28 is the Home command
 
 
-def pick_tool(name, plate):
+def pick_tool(name, insert):
     """
     Writes the tool select command to file (picks between EZ-Seed and 3-in-1)
 
     :param name: name of file
-    :param plate: type of plate
+    :param insert: type of insert
     :return:
     """
 
-    if plate == "EZ-Seed":
+    if insert == "EZ-Seed":
         tool = 0
     else:
         tool = 1
 
     with open("%s.gcode" % name, "a") as file:
         file.write("T%d ;select %s tool \n"
-                   % (tool, plate))  # T is the select tool command
+                   % (tool, insert))  # T is the select tool command
+
+
+def set_absolute(name):
+    """
+    Writes absolute positioning command to file
+
+    :param name: name of file
+    """
+
+    with open("%s.gcode" % name, "a") as file:
+        file.write("G90 ;sets absolute positioning\n"
+                   )
+
+
+def set_relative(name):
+    """
+    Writes relative positioning command to file
+
+    :param name: name of file
+    """
+
+    with open("%s.gcode" % name, "a") as file:
+        file.write("G91 ;sets relative positioning\n"
+                   )
+
+
+def set_mm(name):
+    """
+    Writes command to set units to mm to file
+
+    :param name: name of file
+    """
+
+    with open("%s.gcode" % name, "a") as file:
+        file.write("G21 ;sets units to mm\n"
+                   )
+
+
+def initialization(name, reservoir, rnum, tip, tnum,
+                   plate, insert):
+    """
+    Full initialization of G-code file including writing preferences,
+    selecting tools, and setting to absolute positioning
+
+    :param name: name of the file
+    :param reservoir: Type of reagent reservoir
+    :param rnum: # of reservoirs
+    :param tip: Type of tip
+    :param tnum: # of tips
+    :param plate: Type of culture plate
+    :param insert: Type of custom insert (EZ-Seed or 3-in-1)
+    """
+
+    write_preference(name, reservoir, rnum, tip, tnum, plate, insert)
+    pick_tool(name, insert)
+    set_absolute(name)
+    set_mm(name)
+    home(name)
