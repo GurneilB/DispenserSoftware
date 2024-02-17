0a541a70ae0b6228248e94e35e25b78bba22e2d3
Victoro7 <90275418+Victoro7@users.noreply.github.com>
2024-02-16 19:05:19 -0800

    Added linear/rapid XY functions

--- src/Write_Coordinates.py
+++ src/Write_Coordinates.py
@@ -3,9 +3,9 @@ def rapid_z_pos(rel, name, abs_pos=0, rel_pos=0):
     Writes a rapid Z command to file
 
     :param rel: TRUE if using relative coordinates, FALSE if using absolute coordinates
+    :param name: name of file to write to
     :param abs_pos: absolute coordinate to move to (default = 0)
     :param rel_pos: relative coordinate to move to (default = 0)
-    :param name: name of file to write to
     """
     if rel:
 
@@ -16,7 +16,7 @@ def rapid_z_pos(rel, name, abs_pos=0, rel_pos=0):
             file.write("G0 Z%.3f \n" % abs_pos)
 
 
-def linear_z_pos(rel, name, speed, abs_pos=0, rel_pos=0, ):
+def linear_z_pos(rel, name, speed, abs_pos=0, rel_pos=0):
     """
     Writes a linear Z command to file
 
@@ -40,9 +40,9 @@ def rapid_e_pos(rel, name, abs_pos=0, rel_pos=0):
         Writes a rapid E command to file
 
         :param rel: TRUE if using relative coordinates, FALSE if using absolute coordinates
+        :param name: name of file to write to
         :param abs_pos: absolute coordinate to move to (default = 0)
         :param rel_pos: relative coordinate to move to (default = 0)
-        :param name: name of file to write to
         """
     if rel:
 
@@ -53,7 +53,7 @@ def rapid_e_pos(rel, name, abs_pos=0, rel_pos=0):
             file.write("G0 E%.3f \n" % abs_pos)
 
 
-def linear_e_pos(rel, name, speed, abs_pos=0, rel_pos=0, ):
+def linear_e_pos(rel, name, speed, abs_pos=0, rel_pos=0):
     """
         Writes a linear E command to file
 
@@ -70,3 +70,40 @@ def linear_e_pos(rel, name, speed, abs_pos=0, rel_pos=0, ):
     else:
         with open("%s.gcode" % name, "a") as file:
             file.write("G0 E%.3f F%.3f \n" % (abs_pos, speed))
+
+
+def rapid_xy_pos(rel, name, abs_pos=None, rel_pos=None):
+    """
+        Writes a rapid XY command to file
+
+        :param rel: TRUE if using relative coordinates, FALSE if using absolute coordinates
+        :param name: name of file to write to
+        :param abs_pos: list of X,Y absolute coordinates to move to (default = None)
+        :param rel_pos: list of X,Y relative coordinates to move to (default = None)
+        """
+    if rel:
+
+        with open("%s.gcode" % name, "a") as file:
+            file.write("G0 X%.3f Y%.3f \n" % (rel_pos[1], rel_pos[2]))
+    else:
+        with open("%s.gcode" % name, "a") as file:
+            file.write("G0 X%.3f Y%.3f \n" % (abs_pos[1], abs_pos[2]))
+
+
+def linear_xy_pos(rel, name, speed, abs_pos=None, rel_pos=None):
+    """
+        Writes a rapid XY command to file
+
+        :param rel: TRUE if using relative coordinates, FALSE if using absolute coordinates
+        :param name: name of file to write to
+        :param speed: speed of motor movement
+        :param abs_pos: list of X,Y absolute coordinates to move to (default = None)
+        :param rel_pos: list of X,Y relative coordinates to move to (default = None)
+        """
+    if rel:
+
+        with open("%s.gcode" % name, "a") as file:
+            file.write("G0 X%.3f Y%.3f F%.3f \n" % (rel_pos[1], rel_pos[2], speed))
+    else:
+        with open("%s.gcode" % name, "a") as file:
+            file.write("G0 X%.3f Y%.3f F%.3f \n" % (abs_pos[1], abs_pos[2], speed))
