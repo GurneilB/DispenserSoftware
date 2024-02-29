97d0a274ee618db503bd6aaa93cf3167a3ca8298
aminesmacbook <benhammoumedamine@gmail.com>
2024-02-29 13:47:00 -0800

    tests

--- src/test/tests.py
+++ src/test/tests.py
@@ -1,6 +1,5 @@
 import unittest
 from unittest.mock import mock_open, patch
-import src.main.Exceptions as E
 import src.main.UserInput as UI
 class TestName(unittest.TestCase):
     @patch("builtins.open")
@@ -10,17 +9,17 @@ class TestName(unittest.TestCase):
 
     @patch("builtins.open")
     def test_plate_exception(self, mock_open):
-        plate = E.Exception_plate()
+        plate = UI.get_plate_type()
         self.assertIn(plate, {6, 96})
 
     @patch("builtins.open")
     def test_reservoir_exception(self, mock_open):
-        tube = E.Exception_reservoir()
+        tube = UI.get_reservoir_type()
         self.assertIn(tube, {"A","B"})
 
     @patch("builtins.open")
     def test_tip_exception(self, mock_open):
-        tip = E.Exception_tip()
+        tip = UI.get_tip_type()
         self.assertIsInstance(tip, int)
 
 
