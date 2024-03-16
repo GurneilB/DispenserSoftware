31b828bc88ce1ccfd55da88fd167f8d968e09695
Victoro7 <90275418+Victoro7@users.noreply.github.com>
2024-03-15 17:52:52 -0700

    4 Tip dispensing and aspiration fully supported for 25mL and 1.5 mL reservoirs

--- src/main/Aspiration.py
+++ src/main/Aspiration.py
@@ -163,7 +163,6 @@ def asp_2tip_25ml(name: str, r_vol: list[float], insert: str, tip: int, t_vol=No
     return t_vol
 
 
-# *** FROM LINE 199 NOT UPDATED ***
 def asp_4tip_1_5ml(name: str, r_vol: list[float], insert: str, tip: int, t_vol=None, disp_pos=val.plate_96):
     """
     Writes 4 tip, 1.5mL reservoir Aspiration-related commands to G-code file
@@ -190,11 +189,13 @@ def asp_4tip_1_5ml(name: str, r_vol: list[float], insert: str, tip: int, t_vol=N
         wc.rapid_z_pos(name, val.movement_height_1_5ml)
         wc.rapid_xy_pos(name, val.tubes4tips[0])
         wc.rapid_z_pos(name, val.aspirate_height_1_5ml)
+        k = 0
 
     else:
         wc.rapid_z_pos(name, val.movement_height_1_5ml)
         wc.rapid_xy_pos(name, val.tubes4tips[1])
         wc.rapid_z_pos(name, val.aspirate_height_1_5ml)
+        k = 4
 
     # Run aspiration for both motors
     for i in range(2):
@@ -205,45 +206,132 @@ def asp_4tip_1_5ml(name: str, r_vol: list[float], insert: str, tip: int, t_vol=N
         # Write aspiration command, change remaining reservoir volume
 
         # When reservoir volume is greater than total tip volume, aspirate total tip volume
-        if r_vol[0] >= tip * val.tip4:
+        if r_vol[k + (i * 2)] >= tip:
             init.set_absolute(name)
             wc.rapid_e_pos(name, calc.convert_vol(tip))
 
             # Update t_vol
             t_vol[i] = tip
             # Update r_vol
-            r_vol[0] = r_vol[0] - (t_vol[i] * 2)
+            r_vol[k + (i * 2)] = r_vol[k + (i * 2)] - (t_vol[i])
+            r_vol[k + 1 + (i * 2)] = r_vol[k + 1 + (i * 2)] - (t_vol[i])
 
         # When reservoir volume is less than total tip volume, aspirate remaining reagent
         else:
-            # Set to relative positioning
             init.set_relative(name)
+            wc.rapid_e_pos(name, calc.convert_vol(r_vol[k + (i * 2)]))
 
-            # Aspiration for second motor
-            if i == 1:
-                wc.rapid_e_pos(name, calc.convert_vol(r_vol[0]) / 2)
+            # Update t_vol
+            t_vol[i] = r_vol[k + (i * 2)]
 
-                # Update t_vol
-                t_vol[i] = r_vol[0] / 2
-
-                # Update r_vol
-                r_vol[0] = r_vol[0] - r_vol[0]
-
-            # Aspiration for first motor
-            else:
-                wc.rapid_e_pos(name, calc.convert_vol(r_vol[0] / 4))
-
-                # Update t_vol
-                t_vol[i] = r_vol[0] / 4
-
-                # Update r_vol
-                r_vol[0] = r_vol[0] - t_vol[i] * 2
+            # Update r_vol
+            r_vol[k + (i * 2)] = r_vol[k + (i * 2)] - r_vol[k + (i * 2)]
+            r_vol[k + 1 + (i * 2)] = r_vol[k + 1 + (i * 2)] - r_vol[k + 1 + (i * 2)]
 
     # Set to absolute positioning
     init.set_absolute(name)
 
     # Move to dispensing position
-    wc.rapid_z_pos(name, val.movement_height_25mL)
+    wc.rapid_z_pos(name, val.movement_height_1_5ml)
     wc.rapid_xy_pos(name, disp_pos)
 
     return t_vol
+
+
+def asp_2tip_1_5ml(name: str, r_vol: list[float], insert: str, tip: int, t_vol=None, disp_pos=val.plate_96):
+    """
+    Writes 2 tip, 1.5mL reservoir Aspiration-related commands to G-code file
+
+    :param name: name of file to write to
+    :param r_vol: volume remaining in reservoir
+    :param insert: type of insert used for procedure
+    :param tip: max volume of tip
+    :param t_vol: Current volume of reagent in tip
+    :param disp_pos: Next XY dispensing position
+    :return t_vol: Updated volume of reagent in tip
+    """
+
+    # Write aspiration comment to file, initialize t_vol if None
+    if t_vol is None:
+        t_vol = [0.0, 0.0]
+    init.set_asp(name)
+
+    # Set to absolute positioning
+    init.set_absolute(name)
+
+    # Travel to XYZ pos of reservoirs
+    if r_vol[0] != 0:
+        wc.rapid_z_pos(name, val.movement_height_1_5ml)
+        wc.rapid_xy_pos(name, val.tubes2tips[0])
+        wc.rapid_z_pos(name, val.aspirate_height_1_5ml)
+        k = 0
+
+    elif r_vol[1] != 0:
+        wc.rapid_z_pos(name, val.movement_height_1_5ml)
+        wc.rapid_xy_pos(name, val.tubes4tips[1])
+        wc.rapid_z_pos(name, val.aspirate_height_1_5ml)
+        k = 1
+
+    elif r_vol[4] != 0:
+        wc.rapid_z_pos(name, val.movement_height_1_5ml)
+        wc.rapid_xy_pos(name, val.tubes4tips[4])
+        wc.rapid_z_pos(name, val.aspirate_height_1_5ml)
+        k = 4
+
+    else:
+        wc.rapid_z_pos(name, val.movement_height_1_5ml)
+        wc.rapid_xy_pos(name, val.tubes4tips[5])
+        wc.rapid_z_pos(name, val.aspirate_height_1_5ml)
+        k = 6
+
+    # Run aspiration for both motors
+    for i in range(2):
+
+        # Pick active tool
+        init.pick_tool(name, insert, i)
+
+        # Write aspiration command, change remaining reservoir volume
+
+        # When reservoir volume is greater than total tip volume, aspirate total tip volume
+        if r_vol[k + (i * 2)] >= tip:
+            init.set_absolute(name)
+            wc.rapid_e_pos(name, calc.convert_vol(tip))
+
+            # Update t_vol
+            t_vol[i] = tip
+            # Update r_vol
+            r_vol[k + (i * 2)] = r_vol[k + (i * 2)] - (t_vol[i])
+
+        # When reservoir volume is less than total tip volume, aspirate remaining reagent
+        else:
+            init.set_relative(name)
+            wc.rapid_e_pos(name, calc.convert_vol(r_vol[k + (i * 2)]))
+
+            # Update t_vol
+            t_vol[i] = r_vol[k + (i * 2)]
+
+            # Update r_vol
+            r_vol[k + (i * 2)] = r_vol[k + (i * 2)] - r_vol[k + (i * 2)]
+
+    # Set to absolute positioning
+    init.set_absolute(name)
+
+    # Move to dispensing position
+    wc.rapid_z_pos(name, val.movement_height_1_5ml)
+    wc.rapid_xy_pos(name, disp_pos)
+
+    return t_vol
+
+
+def asp_4tip(name: str, r_vol: list[float], insert: str, tip: int, restype, t_vol=None, disp_pos=val.plate_96):
+    if restype == val.res_25mL:
+        return asp_4tip_25ml(name, r_vol, insert, tip, t_vol, disp_pos)
+    else:
+        return asp_4tip_1_5ml(name, r_vol, insert, tip, t_vol, disp_pos)
+
+
+def asp_2tip(name: str, r_vol: list[float], insert: str, tip: int, restype, t_vol=None, disp_pos=val.plate_96):
+    if restype == val.res_25mL:
+        return asp_2tip_25ml(name, r_vol, insert, tip, t_vol, disp_pos)
+    else:
+        return asp_2tip_1_5ml(name, r_vol, insert, tip, t_vol, disp_pos)
--- src/main/Dispensing.py
+++ src/main/Dispensing.py
@@ -6,15 +6,16 @@ import src.main.Aspiration as asp
 import numpy as np
 
 
-def ez_4tip_dispense(name: str, r_vol: list, tip: int, vol_array: np.array, t_vol, insert="EZ-Seed"):
+def ez_4tip_dispense(name: str, r_vol: list, tip: int, vol_array: np.array, t_vol, restype, insert="EZ-Seed"):
     """
-    Writes Protocol 0 (1x4) Dispensing commands to file
+    Writes EZ-Seed dispensing commands to file for 4-tip dispensing
 
     :param name: name of file to write to
     :param r_vol: volume of reagent in reservoirs
     :param tip: max volume of tip
     :param vol_array: Volumetric design of plate
     :param t_vol: Current volume of reagent in tip
+    :param restype: Type of reservoir used in procedure
     :param insert: type of plate insert
     """
 
@@ -70,7 +71,7 @@ def ez_4tip_dispense(name: str, r_vol: list, tip: int, vol_array: np.array, t_vo
                 pos = [val.plate_96[val.x], val.plate_96[val.y] + (min(k, m) * 9)]
             else:
                 pos = [val.plate_96[val.x] + 9, val.plate_96[val.y] + ((-min(k, m) + 23) * 9)]
-            asp.aspirate(name, r_vol, insert, tip, t_vol, disp_pos=pos)
+            asp.asp_4tip(name, r_vol, insert, tip, restype, t_vol, disp_pos=pos)
             init.set_disp(name)
 
         # Move to next unfilled non-zero well
@@ -81,11 +82,86 @@ def ez_4tip_dispense(name: str, r_vol: list, tip: int, vol_array: np.array, t_vo
             wc.rapid_z_pos(name, val.plate96_movement_height)
             wc.rapid_xy_pos(name, [val.plate_96[val.x] + 9, val.plate_96[val.y] + ((-min(k, m) + 23) * 9)])
 
-def n_ez_4tip_dispense(name: str, r_vol: list, tip: int, vol_array: np.array, t_vol, insert="EZ-Seed"):
-    return None
 
-# *** FROM LINE 112 - UPDATE TO SNAKE 4 COLUMNS ****
-# *** NOT YET COMPATIBLE WITH ASPIRATE FUNCTION (Aspirate acts like there are 4 tips) ***
+def n_ez_4tip_dispense(name: str, r_vol: list, tip: int, vol_array: np.array, t_vol, restype, insert="EZ-Seed"):
+    """
+    Writes 96-well plate dispensing commands to file for 4-tip dispensing
+
+    :param name: name of file to write to
+    :param r_vol: volume of reagent in reservoirs
+    :param tip: max volume of tip
+    :param vol_array: Volumetric design of plate
+    :param t_vol: Current volume of reagent in tip
+    :param restype: Type of reservoir used in procedure
+    :param insert: type of plate insert
+    """
+
+    # Build List of dispensing volumes
+    path0 = vol_array[:, [-2, -1]]
+    path1 = vol_array[:, [2, 3]]
+    snake0 = calc.build_snake(path0)
+    snake1 = calc.build_snake(path1)
+    snake = [snake0, snake1]
+
+    # Move to dispensing start point
+    wc.rapid_xy_pos(name, val.plate_96)
+
+    # Dispense volumes for each well
+    for i in range(len(snake0)):
+
+        # Proceed to next non-zero well
+        if snake[0][i] == 0 and snake[1][i] == 0:
+            continue
+
+        # Move to dispense height
+        wc.rapid_z_pos(name, val.dispense_height_96)
+        init.set_relative(name)
+
+        # Dispense Reagent for both tools
+        for j in range(2):
+            init.pick_tool(name, insert, j)
+            wc.rapid_e_pos(name, calc.convert_vol(-snake[j][i]))
+            t_vol[j] = t_vol[j] - snake[j][i]
+
+        init.set_absolute(name)
+
+        # End procedure at the final well
+        if i == 23:
+            break
+
+        # Find next non-zero well
+        k = i + 1
+        for k in range(i + 1, len(snake[0])):
+            if snake[0][k] != 0:
+                k = k
+                break
+
+        m = i + 1
+        for m in range(i + 1, len(snake[1])):
+            if snake[1][m] != 0:
+                m = m
+                break
+
+        # Aspirate more reagent if current tip volume is too low for next non-zero well, move to next well
+        if snake[min(k, m)] > t_vol[0]:
+            if min(k, m) < 12:
+                pos = [val.plate_96[val.x], val.plate_96[val.y] + (min(k, m) * 9)]
+            else:
+                pos = [val.plate_96[val.x] + 9, val.plate_96[val.y] + ((-min(k, m) + 23) * 9)]
+            asp.asp_4tip(name, r_vol, insert, tip, restype, t_vol, disp_pos=pos)
+            init.set_disp(name)
+
+        # Move to next unfilled non-zero well
+        elif min(k, m) < 12:
+            wc.rapid_z_pos(name, val.plate96_movement_height)
+            wc.rapid_xy_pos(name, [val.plate_96[val.x], val.plate_96[val.y] + (min(k, m) * 9)])
+        else:
+            wc.rapid_z_pos(name, val.plate96_movement_height)
+            wc.rapid_xy_pos(name, [val.plate_96[val.x] + 9, val.plate_96[val.y] + ((-min(k, m) + 23) * 9)])
+
+
+# *** FROM LINE 187 - UPDATE TO SNAKE 4 COLUMNS ****
+# *** UPDATE DISPENSING AND ASPIRATION ***
 def ez_2tip_dispense(name: str, r_vol: list, tip: int, vol_array: np.array, t_vol, insert="EZ-Seed"):
     """
         Writes Protocol 0 (1x4) Dispensing commands to file
@@ -160,6 +236,7 @@ def ez_2tip_dispense(name: str, r_vol: list, tip: int, vol_array: np.array, t_vo
         else:
             wc.rapid_z_pos(name, val.plate96_movement_height)
             wc.rapid_xy_pos(name, [val.plate_96[val.x] + 9, val.plate_96[val.y] + ((-min(k, m) + 23) * 9)])
+
 
 def n_ez_2tip_dispense(name: str, r_vol: list, tip: int, vol_array: np.array, t_vol, insert="EZ-Seed"):
     return None
