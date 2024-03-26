e859174d5bb162a05c8e2174aa485334f7595a60
aminesmacbook <benhammoumedamine@gmail.com>
2024-03-26 10:18:08 -0700

    dropdowns + exit

--- src/main/GUI.py
+++ src/main/GUI.py
@@ -7,43 +7,66 @@ root.title("FluidCAM")
 
 # Add entry field for procedure name
 
+# Storing the selection of the dropdowns
+selections = {
+    'plate': None,
+    'insert': None,
+    'reservoir': None
+}
+def update_selection(name, value):
+    selections[name] = value #assigns a variable using user's choice
+
 # Add Dropdowns
-def dropdown(prompt, options):
+def dropdown(name, prompt, options):
     var = tk.StringVar(root)
     var.set(prompt) # default value
+    def callback(value):
+        update_selection(name, value)
 
-    w = tk.OptionMenu(root, var, *options)
+    w = tk.OptionMenu(root, var, *options, command= callback)
     w.pack() #creates and packs dropdown menu
 
-# Plate Size
+### Plate Size
 opt_plate = ["6 well plate", "96 well plate"]
 p_plate = ["Choose a tip type"]
 
-dropdown(p_plate, opt_plate)
+dropdown('plate',p_plate, opt_plate)
 
-# Insert type (is this needed?)
+### Insert type (is this needed?)
 opt_insert = ["EZ-seed", "3-in-1"]
 p_insert = ["Choose an insert type"]
 
-dropdown(p_insert, opt_insert)
+dropdown('insert',p_insert, opt_insert)
 
-# Reservoir size
+### Reservoir size
 opt_res = ["5mL", "25mL"]
 p_res = ["Choose a reservoir type"]
 
-dropdown(p_res, opt_res)
+dropdown('reservoir',p_res, opt_res)
+
+
+#exit button
+
+def on_exit():
+    print(f"Selected plate size: {selections['plate']}")
+    print(f"Selected insert size: {selections['insert']}")
+    print(f"Selected reservoir size: {selections['reservoir']}")
+    root.destroy()
+
+button = tk.Button(root, text="Exit", command=on_exit)
+button.pack()
 
 # Tip Type
 
 # Add Checkboxes for tip equip and tip eject
 
 # Add "Run Procedure" Button (builds file)
-# Add save button
 # Add save "preferences button"
 
 # Add in-program grid that simulates 96-well plate, saves as np matrix
 
 # Add file menu items
+# Add Save
 
 # Preferences are saved as custom file format (create it)
 
