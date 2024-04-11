5c3bafff9255380cbde096973a88cac85312b3dc
Victoro7 <90275418+Victoro7@users.noreply.github.com>
2024-04-10 19:39:36 -0700

    added ConfigCalibration.py

---
+++ src/main/ConfigCalibration.py
@@ -0,0 +1,35 @@
+
+
+# 96 Well Plate Coordinates
+plate_96 = [125, 20]
+plate96_movement_height = 49
+dispense_height_EZ = 53
+dispense_height_96 = 0  # Not Calibrated
+
+# 6 Well Plate Coordinates
+plate_6 = [101, 0]  # Not Calibrated
+plate6_movement_height = 0  # Not Calibrated
+dispense_height_3in1 = 0  # Not Calibrated
+dispense_height_6 = 0  # Not Calibrated
+
+# 25 mL Reservoir Coordinates
+pos_reservoir_25ml = [6, 61]
+aspirate_height_25ml = 45
+movement_height_25mL = 22
+
+# 1.5 mL Reservoir Coordinates
+tubes4tips = [[11, 104], [47, 104]]  # Not calibrated
+tubes2tips = [[11, 104], [47, 104], [2, 117], [38, 117]]  # Not Calibrated
+aspirate_height_1_5ml = 51  # Not Calibrated
+movement_height_1_5ml = 43  # Not Calibrated
+
+# Ejection Coordinates
+eject_bowl = [5, 108]
+eject_height = 66
+
+# Equipping Coordinates
+tip_tray_8 = [65, 0]  # Not Calibrated
+equip_height = 0  # Not Calibrated
+
+cal_tubes8 = [[9, 104], [27, 104], [45, 104], [72, 104],
+              [0, 117], [18, 117], [36, 117], [54, 117]]  # Coordinates for 1-Tip dispensing
