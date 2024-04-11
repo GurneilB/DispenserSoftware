69ab8e0576dcdea6caf7dfd7385b5e0183b67ced
Victoro7 <90275418+Victoro7@users.noreply.github.com>
2024-04-11 07:51:53 -0700

    New Positional calibration added

--- src/main/ConfigCalibration.py
+++ src/main/ConfigCalibration.py
@@ -1,10 +1,8 @@
-
-
 # 96 Well Plate Coordinates
-plate_96 = [125, 20]
-plate96_movement_height = 49
-dispense_height_EZ = 53
-dispense_height_96 = 0  # Not Calibrated
+plate_96 = [125, 21]
+plate96_movement_height = 30
+dispense_height_EZ = 40
+dispense_height_96 = 40  # Not Calibrated
 
 # 6 Well Plate Coordinates
 plate_6 = [101, 0]  # Not Calibrated
@@ -13,12 +11,12 @@ dispense_height_3in1 = 0  # Not Calibrated
 dispense_height_6 = 0  # Not Calibrated
 
 # 25 mL Reservoir Coordinates
-pos_reservoir_25ml = [6, 61]
+pos_reservoir_25ml = [6, 49]
 aspirate_height_25ml = 45
-movement_height_25mL = 22
+movement_height_25mL = 18
 
 # 1.5 mL Reservoir Coordinates
-tubes4tips = [[11, 104], [47, 104]]  # Not calibrated
+tubes4tips = [[11, 101], [2, 114]]  # Not calibrated
 tubes2tips = [[11, 104], [47, 104], [2, 117], [38, 117]]  # Not Calibrated
 aspirate_height_1_5ml = 51  # Not Calibrated
 movement_height_1_5ml = 43  # Not Calibrated
--- src/main/Value.py
+++ src/main/Value.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+
 """ Values related to the program's operation. Includes calibrated positions"""
 
 # Coordinate Types
@@ -26,44 +27,6 @@ _1_5mL = 1500.000
 _25mL = 25000.000
 _250uL_tip = 250
 
-# tree = ET.parse("calibration_values.xml")
-# root = tree.getroot()
-
-# 96 Well Plate Coordinates
-_96_well_coordinates =[125,21] # [int(root.find(plate_96).attrib['x']), int(root.find(plate_96).attrib['y'])]
-_96_well_movement_height = 30 #int(root.find(plate_96).attrib['z_movement'])
-ez_dispense_height = 40 #int(root.find(plate_96).attrib['z']) - 3
-_96_well_dispense_height = 40 #int(root.find(plate_96).attrib['z'])
-
-# 6 Well Plate Coordinates
-_6_well_coordinates = [101, 0]  # Not Calibrated
-_6_well_movement_height = 0  # Not Calibrated
-dispense_height_3in1 = 0  # Not Calibrated
-dispense_height_6 = 0  # Not Calibrated
-
-# 25 mL Reservoir Coordinates
-pos_reservoir_25ml = [6, 49]
-aspirate_height_25ml = 45
-movement_height_25mL = 18
-
-# 1.5 mL Reservoir Coordinates
-tubes4tips = [[11, 104], [47, 104]]  # Not calibrated
-tubes2tips = [[11, 104], [47, 104], [2, 117], [38, 117]]  # Not Calibrated
-aspirate_height_1_5ml = 51  # Not Calibrated
-movement_height_1_5ml = 43  # Not Calibrated
-
-# Ejection Coordinates
-eject_bowl = [5, 108]
-eject_height = 66
-
-# Presenting Coordinates
-present = [65, 0]
-present_height = 0
-
-# Equipping Coordinates
-tip_tray_8 = [65, 0]  # Not Calibrated
-equip_height = 0  # Not Calibrated
-
 # 96-well plate dimensions
 dims_96 = (8, 12)
 
@@ -87,22 +50,67 @@ rnum8 = 8
 x = 0
 y = 1
 
+""" COORDINATES AND CALIBRATION MODEL"""
+tree = ET.parse("calibration_values.xml")
+root = tree.getroot()
+
+# 96 Well Plate Coordinates
+_96_well_coordinates = [int(root.find("plate_96").attrib['x']), int(root.find("plate_96").attrib['y'])]
+_96_well_movement_height = int(root.find("plate_96").attrib['zmove'])
+ez_dispense_height = int(root.find("plate_96").attrib['z']) - 3
+_96_well_dispense_height = int(root.find("plate_96").attrib['z'])
+
+# 6 Well Plate Coordinates
+_6_well_coordinates = [int(root.find("plate_6").attrib['x']), int(root.find("plate_6").attrib['y'])]
+_6_well_movement_height = int(root.find("plate_6").attrib['zmove'])
+dispense_height_3in1 = int(root.find("plate_6").attrib['z']) - 3
+dispense_height_6 = int(root.find("plate_6").attrib['z'])
+
+# 25 mL Reservoir Coordinates
+pos_reservoir_25ml = [int(root.find("pos_reservoir_25ml").attrib['x']),
+                      int(root.find("pos_reservoir_25ml").attrib['y'])]
+aspirate_height_25ml = int(root.find("pos_reservoir_25ml").attrib['z'])
+movement_height_25mL = int(root.find("pos_reservoir_25ml").attrib['zmove'])
+
+# 1.5 mL Reservoir Coordinates
+tubes4tips = [[int(root.find("tubes4tips").attrib['x']), int(root.find("tubes4tips").attrib['y'])],
+              [int(root.find("tubes4tips").attrib['x']) - 9,
+               int(root.find("tubes4tips").attrib['y']) + 13]]  # Not calibrated
+tubes2tips = [[int(root.find("tubes4tips").attrib['x']), int(root.find("tubes4tips").attrib['y'])],
+              [int(root.find("tubes4tips").attrib['x']) + 36, int(root.find("tubes4tips").attrib['y'])],
+              [int(root.find("tubes4tips").attrib['x']) - 9, int(root.find("tubes4tips").attrib['y']) + 13],
+              [int(root.find("tubes4tips").attrib['x']) + 27, int(root.find("tubes4tips").attrib['y']) + 13]]
+aspirate_height_1_5ml = int(root.find("tubes4tips").attrib['z'])
+movement_height_1_5ml = int(root.find("tubes4tips").attrib['zmove'])
+
+# Ejection Coordinates
+eject_bowl = [int(root.find("eject_bowl").attrib['x']), int(root.find("eject_bowl").attrib['y'])]
+eject_height = int(root.find("eject_bowl").attrib['z'])
+
+# Equipping Coordinates
+tip_tray_8 = [int(root.find("tip_tray_8").attrib['x']), int(root.find("tip_tray_8").attrib['y'])]
+equip_height = int(root.find("tip_tray_8").attrib['z'])
+
+# Presenting Coordinates
+present = [65, 0]
+present_height = 0
+
+# New Calibration Coordinates
+cal_25_pos_new = [0, 116]
+cal_tubes_new = [[11, 101], [2, 114]]
+
+# Calibration model dispensing factor
+model_factor = 0.163
+
+""" OLD CALIBRATION VALUES"""
 # Calibration and Testing Coordinates
 beaker = [23.5, 61]
 beaker_asp_height = 55
 cal_tubes8 = [[9, 104], [27, 104], [45, 104], [72, 104],
               [0, 117], [18, 117], [36, 117], [54, 117]]  # Coordinates for 1-Tip dispensing
-cal_tubes4 = [[10, 104], [46, 104], [37, 117],[1, 117]]  # Coordinates for 2-Tip dispensing
-cal_tubes2 = [[11, 101], [2,114]]  # Coordinates for 4-Tip dispensing
+cal_tubes4 = [[10, 104], [46, 104], [37, 117], [1, 117]]  # Coordinates for 2-Tip dispensing
+cal_tubes2 = [[11, 101], [2, 114]]  # Coordinates for 4-Tip dispensing
 tubes_disp_height = 78
 cal_movement_height = 17
 dispense_move_height = 41
 zero_height = 0  # Find height, set manually with G92 E
-
-# Calibration model dispensing factor
-model_factor = 0.163
-
-# New Calibration Coordinates
-cal_25_pos_new = [0, 116]
-cal_tubes_new = [[11, 101], [2,114]]
-
