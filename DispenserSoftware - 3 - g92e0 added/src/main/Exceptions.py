e3b6a3c4025fb5f2be035ed46261d428d5acf3d4
GurneilB <91935911+GurneilB@users.noreply.github.com>
2024-12-02 16:20:40 -0800

    Delete DispenserSoftware - 3 - g92e0 added directory

--- DispenserSoftware - 3 - g92e0 added/src/main/Exceptions.py
+++
@@ -1,8 +0,0 @@
-
-class OutOfRangeError(Exception):
-    def __init__(self, value, minimum, maximum):
-        self.value = value
-        self.minimum = minimum
-        self.maximum = maximum
-        message = f"Please enter a volume between [{minimum}, {maximum}]. {value} is out of the range. "
-        super().__init__(message)
--- DispenserSoftware - 3 - g92e0 added/src/test/CalculationsTests.py
+++
@@ -1,4 +0,0 @@
-import unittest
-from unittest.mock import mock_open, patch
-import src.main.Calculations as calc
-import src.main.Value as val
